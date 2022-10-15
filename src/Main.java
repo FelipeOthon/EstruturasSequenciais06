61292175f86fd9350cc1c401dcae9a2ca66127e1
FelipeOthon <felipeosantiago@gmail.com>
2022-10-14 23:15:21 -0300

    enviar readme

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat();
+
+        double x;
+
+        System.out.println("Qual o raio do circulo:");
+        x = sc.nextDouble();
+
+
+        double z = Math.PI * Math.pow(x,2 );
+
+        System.out.println("a area do circulo e :" + df.format(z )  + "cm2" );
     }
 }
